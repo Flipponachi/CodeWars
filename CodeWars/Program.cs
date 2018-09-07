a7e687c86e05391ab4d6c79168426eb3aa1e9719
Flipponachi <iamflipponachi@gmail.com>
2018-09-07 15:01:30 +0100

    Solved the Isogram challenge

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace CodeWars
 {
@@ -10,6 +11,37 @@ namespace CodeWars
     {
         static void Main(string[] args)
         {
+            WriteLine(CodeWars.IsIsogram("MooOse"));
+
+            ReadLine();
         }
     }
+
+       //An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+       // Implement a function that determines whether a string that contains only letters is an isogram.
+       // Assume the empty string is an isogram. 
+       // Ignore letter case.
+
+    static class CodeWars
+    {
+        public static bool IsIsogram(string str)
+        {
+            //Check the string length, if length is different from char array
+            //then return false
+            int stringLength = str.ToLower().Length;
+            int stringArrayLength = new String(str.ToLower().Distinct().ToArray()).Length;
+
+            if (stringLength == stringArrayLength)
+            {
+                return true;
+            }
+            else if (stringLength > stringArrayLength)
+            {
+                return false;
+            }
+            return true;
+        } 
+    }
+
+
 }
