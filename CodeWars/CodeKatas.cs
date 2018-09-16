5657f8068abd98f13620bde339cd8b0e57ddc480
Flipponachi <iamflipponachi@gmail.com>
2018-09-16 04:41:42 +0100

    Removed unnecessary folders

--- CodeWars/CodeKatas.cs
+++ CodeWars/CodeKatas.cs
@@ -41,5 +41,62 @@ namespace CodeWars
             }
             return false;
         }
+
+        /*
+         * A digital root is the recursive sum of all the digits in a number.
+         * Given n, take the sum of the digits of n.
+         * If that value has two digits, continue reducing in this way until a single-digit number is produced.
+         * This is only applicable to the natural numbers.
+         * digital_root(16)
+            => 1 + 6
+            => 7
+            digital_root(942)
+            => 9 + 4 + 2
+            => 15 ...
+            => 1 + 5
+            => 6
+            
+            digital_root(132189)
+            => 1 + 3 + 2 + 1 + 8 + 9
+            => 24 ...
+            => 2 + 4
+            => 6
+            
+            digital_root(493193)
+            => 4 + 9 + 3 + 1 + 9 + 3
+            => 29 ...
+            => 2 + 9
+            => 11 ...
+            => 1 + 1
+            => 2
+         */
+        
+        public int DigitalRoot(long n)
+        {
+            //Split into character array
+            
+            string inputValueString = n.ToString();
+            var stringChar = inputValueString.ToCharArray();
+            
+            //value of input
+            int recursiveSum = 0;
+            
+
+            for (int i = 0; i < stringChar.Length; i++)
+            {
+                int currentValue = (int)char.GetNumericValue(stringChar[i]);
+                recursiveSum += currentValue;
+                
+            }
+
+            string valueOfRecursiveSum = recursiveSum.ToString();
+            var lengtOfRecursiveArray = valueOfRecursiveSum.ToCharArray().Length;
+            while (lengtOfRecursiveArray > 1)
+            {
+                return DigitalRoot(recursiveSum);
+            }
+           
+            return recursiveSum;
+        }
     }
 }
--- CodeWars/CodeWarsTests/CodeKatasTests.cs
+++ CodeWars/CodeWarsTests/CodeKatasTests.cs
@@ -6,6 +6,13 @@ namespace CodeWars.CodeWarsTests
     [TestFixture]
     public class CodeKatasTests
     {
+        private CodeKatas _codeKatas;
+
+        [SetUp]
+        public void Setup()
+        {
+            _codeKatas = new CodeKatas();
+        }
         [Test]
         [TestCase("aabaa", true)]
         [TestCase("abac", false)]
@@ -14,10 +21,20 @@ namespace CodeWars.CodeWarsTests
         [TestCase("zzzazzazz", false)]
         public void IsPalindrome_DifferentValue_ReturnsCorrectValue(string value, bool expectedResult)
         {
-            var codeKatas = new CodeKatas();
-            var result = codeKatas.IsPalindrome(value);
+           
+            var result = _codeKatas.IsPalindrome(value);
             
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(16, 7)]
+        [TestCase(942, 6)]
+        [TestCase(132189, 6)]
+        public void DigitalRoot_InputValue_ReturnsFirstRecursiveValue(int value, int expectedValue)
+        {
+            var result = _codeKatas.DigitalRoot(value);
+            Assert.That(result, Is.EqualTo(expectedValue));
         }
         
     }
